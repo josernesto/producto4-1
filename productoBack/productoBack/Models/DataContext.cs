2e0d230703275f21932ce3be9c5b7f0f45972f18
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <a2017112569@estudiantes.upsa.edu.bo>
2019-03-16 13:45:08 -0400

    commit completo

---
+++ productoBack/productoBack/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace productoBack.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+            
+        }
+
+        public System.Data.Entity.DbSet<productoBack.Models.Product> Products { get; set; }
+    }
+}
---
+++ productoBack/productoBack/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace productoBack.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name="Product Name")]
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
